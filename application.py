2c7eefec353429fd5b7adfc5be5fec6e55c2129a
marcosxtyo <112781600+marcosxtyo@users.noreply.github.com>
2022-12-15 10:52:04 +0100

    first app commit

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 # print a nice greeting.
 def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+    return '<p>Bye %s!</p>\n' % username
 # some bits of text for the page.
 header_text = '''
 <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
@@ -10,7 +10,7 @@ instructions = '''
 <p><em>Hint</em>: This is a RESTful web service! Append a
 username
 to the URL (for example: <code>/Thelonious</code>) to say
-hello to
+bye to
 someone specific.</p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
